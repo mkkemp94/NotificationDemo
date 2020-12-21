60e8a4eedbde52ae94e50aa63dd2d825e7c7b723
MKemp <mkemp@accu-time.com>
2020-12-21 15:46:59 -0500

    reply to notification

--- app/src/main/java/com/mkemp/notificationdemo/MainActivity.kt
+++ app/src/main/java/com/mkemp/notificationdemo/MainActivity.kt
@@ -9,26 +9,28 @@ import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.app.NotificationCompat
+import androidx.core.app.RemoteInput
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity()
 {
     private val channelID = "com.mkemp.notificationdemo.channel"
     private var notificationManager: NotificationManager? = null
-    
+    private val KEY_REPLY = "key_reply"
+
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
+
         notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         createNotificationChannel(channelID, "DemoChannel", "this is a demo")
-        
+
         id_button.setOnClickListener {
             displayNotification()
         }
     }
-    
+
     private fun displayNotification() {
         val notificationId = 45
         val tapResultIntent = Intent(this, SecondActivity::class.java)
@@ -38,6 +40,18 @@ class MainActivity : AppCompatActivity()
                 tapResultIntent,
                 PendingIntent.FLAG_UPDATE_CURRENT
         )
+
+        // reply action
+        val remoteInput: RemoteInput = RemoteInput.Builder(KEY_REPLY).run {
+            setLabel("Insert your name here")
+            build()
+        }
+
+        val replyAction : NotificationCompat.Action = NotificationCompat.Action.Builder(
+            0,
+            "REPLY",
+            pendingIntent
+        ).addRemoteInput(remoteInput).build()
 
         // action button 1
         val intent2 = Intent(this, DetailsActivity::class.java)
@@ -67,13 +81,14 @@ class MainActivity : AppCompatActivity()
                 .setSmallIcon(android.R.drawable.ic_dialog_info)
                 .setAutoCancel(true)
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
-                .setContentIntent(pendingIntent)
+//                .setContentIntent(pendingIntent)
+                .addAction(replyAction)
                 .addAction(action2)
                 .addAction(action3)
                 .build()
         notificationManager?.notify(notificationId, notification)
     }
-    
+
     private fun createNotificationChannel(id: String, name: String, channelDescription: String) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val importance = NotificationManager.IMPORTANCE_HIGH
@@ -83,5 +98,5 @@ class MainActivity : AppCompatActivity()
             notificationManager?.createNotificationChannel(channel)
         }
     }
-    
+
 }
--- app/src/main/java/com/mkemp/notificationdemo/SecondActivity.kt
+++ app/src/main/java/com/mkemp/notificationdemo/SecondActivity.kt
@@ -1,11 +1,42 @@
 package com.mkemp.notificationdemo
 
+import android.app.Notification
+import android.app.NotificationManager
+import android.app.RemoteInput
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.NotificationCompat
+import kotlinx.android.synthetic.main.activity_second.*
 
 class SecondActivity : AppCompatActivity() {
+
+    private val channelID = "com.mkemp.notificationdemo.channel"
+    private val KEY_REPLY = "key_reply"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+        receiveInput()
+    }
+
+    fun receiveInput() {
+        val intent = this.intent
+        val remoteInput = RemoteInput.getResultsFromIntent(intent)
+        if (remoteInput != null) {
+
+            val inputString = remoteInput.getCharSequence(KEY_REPLY).toString()
+            result_text_view.text = inputString
+
+            val notificationId = 45
+            val repliedNotification = NotificationCompat.Builder(this, channelID)
+                .setSmallIcon(android.R.drawable.ic_dialog_info)
+                .setContentText("Your reply received")
+                .build()
+
+            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.notify(notificationId, repliedNotification)
+
+        }
     }
 }
