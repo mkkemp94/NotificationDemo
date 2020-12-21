988fe998c492fbd66bdc8ba0a0d99ca3f61948e7
MKemp <mkemp@accu-time.com>
2020-12-21 15:12:05 -0500

    add notification action buttons

--- app/src/main/java/com/mkemp/notificationdemo/MainActivity.kt
+++ app/src/main/java/com/mkemp/notificationdemo/MainActivity.kt
@@ -38,6 +38,29 @@ class MainActivity : AppCompatActivity()
                 tapResultIntent,
                 PendingIntent.FLAG_UPDATE_CURRENT
         )
+
+        // action button 1
+        val intent2 = Intent(this, DetailsActivity::class.java)
+        val pendingIntent2: PendingIntent = PendingIntent.getActivity(
+            this,
+            0,
+            intent2,
+            PendingIntent.FLAG_UPDATE_CURRENT
+        )
+        val action2 : NotificationCompat.Action =
+            NotificationCompat.Action.Builder(0, "Details", pendingIntent2).build()
+
+        // action button 2
+        val intent3 = Intent(this, SettingsActivity::class.java)
+        val pendingIntent3: PendingIntent = PendingIntent.getActivity(
+            this,
+            0,
+            intent3,
+            PendingIntent.FLAG_UPDATE_CURRENT
+        )
+        val action3 : NotificationCompat.Action =
+            NotificationCompat.Action.Builder(0, "Settings", pendingIntent3).build()
+
         val notification = NotificationCompat.Builder(this@MainActivity, channelID)
                 .setContentTitle("Demo Title")
                 .setContentText("This is a demo notification")
@@ -45,6 +68,8 @@ class MainActivity : AppCompatActivity()
                 .setAutoCancel(true)
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
                 .setContentIntent(pendingIntent)
+                .addAction(action2)
+                .addAction(action3)
                 .build()
         notificationManager?.notify(notificationId, notification)
     }
